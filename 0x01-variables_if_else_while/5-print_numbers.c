ca29cf807f873f0be3fe7bed9c79f1e7047bb9ed
Princesse19 <munyanezaprincesserita@gmail.com>
2023-04-15 05:32:13 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,12 +4,10 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-        int num;
-
-        for (num = 0; num < 10; num++){
-                printf("%d\n",num);
-        }
-        return 0;
+int main(void){
+	int num;
+	for (num = 0; num < 10; num++){
+		printf("%d",num);
+	}
+	return 0;
 }

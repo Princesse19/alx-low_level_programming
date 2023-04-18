8ce8874d7b9659732cbcb4bfb7f67cb030e6847a
Princesse19 <munyanezaprincesserita@gmail.com>
2023-04-18 08:22:49 -0700

    FILE name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success),print the name of the current file.
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
